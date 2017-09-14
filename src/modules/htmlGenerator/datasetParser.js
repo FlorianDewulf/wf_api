c5c7e2136381184cc72c79e4564f7914a6d04661
FlorianDewulf <florian.dewulf@epitech.eu>
2017-09-14 12:04:49 +0200

    Add function in blade management

--- src/modules/htmlGenerator/datasetParser.js
+++ src/modules/htmlGenerator/datasetParser.js
@@ -1,13 +1,80 @@
+// Like the normal split but don't split in a function or in a string
+function customSplit (string, delimiter) {
+  let splitted = []
+  let beginning = 0
+  let preventSplit = {
+    delimiter: '',
+    activated: false
+  }
+  for (let i = 0; i < string.length; ++i) {
+    if (preventSplit.activated === true &&
+      (
+        (string[i] === '"' && preventSplit.delimiter === '"') ||
+        (string[i] === "'" && preventSplit.delimiter === "'") ||
+        (string[i] === ')' && preventSplit.delimiter === '(')
+      )
+    ) {
+      if (i > 0 && string[i - 1] !== '\\') {
+        preventSplit.delimiter = ''
+        preventSplit.activated = false
+        continue
+      }
+    }
+    if (preventSplit.activated === false && (string[i] === '"' || string[i] === "'" || string[i] === '(')) {
+      preventSplit.delimiter = string[i]
+      preventSplit.activated = true
+      continue
+    }
+    if (preventSplit.activated === false && string[i] === delimiter) {
+      splitted.push(string.substr(beginning, i - beginning))
+      beginning = i + 1
+    }
+  }
+  splitted.push(string.substr(beginning, string.length - beginning))
+  return splitted
+}
+
+// Get the data from a variable
+function getVariableData (currentData, key) {
+  if (typeof currentData[key] === 'undefined' || currentData[key] === null) {
+    return undefined
+  }
+  return currentData[key]
+}
+
+// Execute a function and return the result
+function getFunctionData (currentData, key, datas, aliases) {
+  let functionParameters = key.match(/([A-Za-z0-9_]+)\(([A-Za-z0-9_. ,'"[\]\\]+)\)/)
+  if (functionParameters) {
+    let parameters = customSplit(functionParameters[2], ',')
+    let toEval = 'currentData[key.substr(0, key.indexOf(\'(\'))]('
+    parameters.map((parameter, index) => {
+      if (index !== 0) {
+        toEval += ', '
+      }
+      if (parameter.trim().match(/(\d+)|('.*')|(".*")/)) {
+        toEval += parameter.trim()
+      } else {
+        toEval += "'" + getData(datas, parameter.trim(), aliases) + "'"
+      }
+    })
+    return eval(toEval + ')')
+  } else {
+    console.log('Method with a bad format')
+    return undefined
+  }
+}
+
 // Retrieve the data by a key
 // If the key is a sub object, we go deeper
-function getData(datas, key, aliases) {
+function getData (datas, key, aliases) {
   if (typeof key === 'undefined' || typeof datas === 'undefined' || !datas || !key) {
     return undefined
   }
 
   let currentData = datas
   key = key.replace(/(\[|\])/g, '.')
-  let splittedKey = key.split('.')
+  let splittedKey = customSplit(key, '.')
   // For the loops
   for (let aliasName in aliases) {
     if (splittedKey[0].startsWith(aliasName)) {
@@ -20,10 +87,11 @@ function getData(datas, key, aliases) {
     if (splittedKey[i].length === 0) {
       continue
     }
-    if (typeof currentData[splittedKey[i]] === 'undefined' || currentData[splittedKey[i]] === null) {
-      return undefined
+    if (splittedKey[i].includes('(')) {
+      return getFunctionData(currentData, splittedKey[i], datas, aliases)
+    } else {
+      currentData = getVariableData(currentData, splittedKey[i])
     }
-    currentData = currentData[splittedKey[i]]
   }
   return currentData
 }
@@ -39,10 +107,10 @@ function parseTxt (node, datas, aliases) {
   }
   // to edit
   let innerHtml = node.innerHTML
-  let bladeNotations = innerHtml.match(/\{\{[ \t]*[A-Za-z0-9_\.\[\]]+[ \t]*\}\}/g)
+  let bladeNotations = innerHtml.match(/\{\{[ \t]*[A-Za-z0-9_\.\[\](, )'"]+[ \t]*\}\}/g)
   if (bladeNotations) {
     for (let bladeNotation in bladeNotations) {
-      let internalValue = bladeNotations[bladeNotation].match(/\{\{[ \t]*([A-Za-z0-9_\.\[\]]+)[ \t]*\}\}/)
+      let internalValue = bladeNotations[bladeNotation].match(/\{\{[ \t]*([A-Za-z0-9_\.\[\](, )'"]+)[ \t]*\}\}/)
       let val = getData(datas, internalValue[1], aliases)
       if (typeof val !== 'undefined') {
         innerHtml = innerHtml.replace(bladeNotations[bladeNotation], val)
@@ -61,7 +129,7 @@ function parseLoop (node, datas, aliases) {
   }
   if (node.getAttribute('hg-loop')) {
     // Get the pattern "xx in xxx"
-    let match = node.getAttribute('hg-loop').trim().match(/([A-za-z_]+) in ([A-za-z_]+)/)
+    let match = node.getAttribute('hg-loop').trim().match(/([A-za-z_]+) in ([A-za-z_.[\]()]+)/)
     if (match && match.length === 3) {
       let aliasValue = getData(datas, match[2], aliases)
       // Set up the alias to get the data
@@ -72,7 +140,7 @@ function parseLoop (node, datas, aliases) {
           iteration: 0
         }
         returnValue.isParsed = true
-        
+
         // create container to keep the position in the DOM
         let container = document.createElement('div')
         let generatedId = Math.round(Math.random() * 10000)
